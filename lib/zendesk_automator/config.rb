03dc22dbc09cb7efdd04de215c6270135255c6a8
ondmagi <andreas@superblock.se>
2014-11-28 10:15:06 +0100

    Reload config file on HUP signal

--- lib/zendesk_automator/config.rb
+++ lib/zendesk_automator/config.rb
@@ -1,21 +1,28 @@
 module ZendeskAutomator
   class Config
     attr_reader :zendesk_url, :zendesk_username, :zendesk_token,
-        :schedules, :tasks
+        :schedules, :tasks, :config_path
 
     def read(path)
-      unless File.file?(path)
-        puts "Yikes, #{path} is not a valid file"
-        $logger.warn "Exiting, #{path} is not a valid file!"
+      @config_path = path
+      unless File.file?(@config_path)
+        puts "Yikes, #{@config_path} is not a valid file"
+        $logger.warn "Exiting, #{@config_path} is not a valid file!"
         exit 1
       end
-      values = YAML::load_file(path)
+      values = YAML::load_file(@config_path)
+
+      $logger.debug "Got values from #{@config_path}: #{values}"
 
       @zendesk_url = values['zendesk']['url']
       @zendesk_username = values['zendesk']['username']
       @zendesk_token = values['zendesk']['token']
       @schedules = values.fetch('schedules')
       @tasks = values.fetch('tasks')
+    end
+
+    def reload!
+      read(@config_path)
     end
 
   end
--- lib/zendesk_automator/run.rb
+++ lib/zendesk_automator/run.rb
@@ -3,31 +3,52 @@ module ZendeskAutomator
 
     def initialize(options)
       # Read config
-      config = Config.new
-      config.read(options[:config_path])
+      @config = Config.new
+      @config.read(options[:config_path])
+      @dryrun = options[:dry_run]
 
-      $logger.debug "DRY-RUN: #{options[:dry_run]}"
-      $logger.debug "Found schedules: #{config.schedules}"
-      $logger.debug "Found tasks: #{config.tasks}"
+      #$logger.debug "DRY-RUN: #{options[:dry_run]}"
+      #$logger.debug "Found schedules: #{config.schedules}"
+      #$logger.debug "Found tasks: #{config.tasks}"
 
-      # Setup ZendeskAutomator::Client
-      Client.new(config.zendesk_url, config.zendesk_username, config.zendesk_token)
+      @client = Client.new(@config.zendesk_url, @config.zendesk_username, @config.zendesk_token)
+      @jobs = []
+      @scheduler = Rufus::Scheduler.new
 
-      # Schedule and perform create the tickets based on tasks
-      scheduler = Rufus::Scheduler.new
+      schedule_tasks!(@config)
+
+      trap 'HUP' do
+        t = Thread.new do
+          $logger.info "Reloading config!"
+          @jobs.count.times do
+            job = @jobs.shift
+            $logger.debug "Trying to unschedule #{job}"
+            job.unschedule
+          end
+          @config.reload!
+          schedule_tasks!(@config)
+        end
+      end
+
+      @scheduler.join
+
+    end
+
+    def schedule_tasks!(config)
       config.tasks.each do |task, ticket_params|
         schedule_name = ticket_params['schedule']
         cron_str = config.schedules[schedule_name]['cron']
 
         $logger.debug "Trying to schedule #{task} with cron schedule '#{cron_str}'"
 
-        scheduler.cron "#{cron_str}" do
+        @scheduler.cron "#{cron_str}" do |job|
           t = Ticket.new
-          t.dryrun = true if options[:dry_run]
+          t.dryrun = true if @dryrun
           t.create(ticket_params)
+          @jobs << job
         end
       end
-      scheduler.join
+
     end
 
   end
